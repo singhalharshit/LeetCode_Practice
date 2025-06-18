bea58b2121c5d6f888ac04ae587af6343bab9a74
Harshit Singhal <singhalharshit2910@gmail.com>
2025-06-19 03:48:27 +0530

    More into the code

--- Other_Coding_Practice/problem_8.py
+++ Other_Coding_Practice/problem_8.py
@@ -1,3 +1,11 @@
 """
-    Find the first non repeated character
+    Given a string find the first non repeated character
 """
+
+string = "teeter"
+for i in string:
+    if string.count(i) > 1:
+        pass
+    elif string.count(i) ==1:
+        print(i)
+        break
