1d35788b3aa8bd358878ae85b70bf0aee26d7a81
Harshit Singhal <singhalharshit2910@gmail.com>
2025-06-20 15:47:36 +0530

    more into practice

---
+++ Other_Coding_Practice/problem_14.py
@@ -0,0 +1,10 @@
+"""
+    Write a function to calculate and return the square of a number
+"""
+
+
+def square(x):
+    return x*x
+
+
+print(square(4))
---
+++ Other_Coding_Practice/problem_15.py
@@ -0,0 +1,9 @@
+"""
+    Create a function that takes two numbers as parameters and returns their sum
+"""
+
+def sum(x,y):
+    return x+y
+
+
+print(sum(9,10))
