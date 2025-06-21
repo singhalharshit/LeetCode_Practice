160cc470262a0f8e9621f2ab1440d9c16f18b72f
Harshit Singhal <singhalharshit2910@gmail.com>
2025-06-21 17:57:36 +0530

    more of oops a bit

--- Other_Coding_Practice/problem_23.py
+++ Other_Coding_Practice/problem_23.py
@@ -9,14 +9,17 @@
 
 class Car:
     def __init__(self,brand,model):
-        self.brand = brand
+        self.__brand = brand
         self.model = model
 
+    def get_brand(self):
+        return self.__brand + " !"
+
     def full_name_of_car(self):
-        return f"Full name of the car is: {self.brand} {self.model}"
+        return f"Full name of the car is: {self.__brand} {self.model}"
 
 object_of_class_car = Car("Honda","civic")
-print(object_of_class_car.brand)
+print(object_of_class_car.__brand)
 print(object_of_class_car.model)
 print(object_of_class_car.full_name_of_car())
 
@@ -32,3 +35,10 @@ class ElectricCar(Car):
 
 ElectricCar_Obj = ElectricCar("Tesla","Model S","85kWh")
 print(ElectricCar_Obj.model)
+print(ElectricCar_Obj.full_name_of_car())
+
+"""
+    Modify the Car class to encapsulate the brand attribute, making it private, and provide a getter method for it
+"""
+
+# print(ElectricCar_Obj.get_brand())
